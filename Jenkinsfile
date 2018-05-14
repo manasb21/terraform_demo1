93a32ad3841a2e06e3b5df3c3c7463c88e9d63ff
root <root@ip-172-31-14-238.us-west-1.compute.internal>
2018-05-14 09:21:50 +0000

    first commit2

---
+++ Jenkinsfile
@@ -0,0 +1,48 @@
+pipeline {
+  agent {
+    docker {
+      image 'goforgold/build-container:latest'
+    }
+  }
+  stages {
+    stage('Build') {
+      steps {
+        sh 'npm install'
+      }
+    }
+    stage('Create Packer AMI') {
+        steps {
+          withCredentials([
+            usernamePassword(credentialsId: 'aws_cred', passwordVariable: 'AWS_SECRET', usernameVariable: 'AWS_KEY')
+          ]) {
+	    sh 'cd /root/packer'
+	    sh 'pwd'
+	    sh 'hostname -i'
+            sh 'packer build -var aws_access_key=${AWS_KEY} -var aws_secret_key=${AWS_SECRET} packer.json'
+        }
+      }
+    }
+    stage('AWS Deployment') {
+      steps {
+          withCredentials([
+            usernamePassword(credentialsId: 'aws_cred', passwordVariable: 'AWS_SECRET', usernameVariable: 'AWS_KEY'),
+            usernamePassword(credentialsId: 'git', passwordVariable: 'REPO_PASS', usernameVariable: 'REPO_USER'),
+          ]) {
+            sh 'rm -rf terraform_state_file'
+            sh 'git clone https://github.com/manasb21/terraform_state_file.git'
+            sh '''
+               cd terraform_state_file
+               terraform init
+               terraform apply -auto-approve -var access_key=${AWS_KEY} -var secret_key=${AWS_SECRET}
+               git add terraform.tfstate
+               git -c user.name="Manas Biswal" -c user.email="biswalmanas21@gmail.com" commit -m "terraform state update from Jenkins"
+               git push https://${REPO_USER}:${REPO_PASS}@github.com/manasb21/terraform_state_file.git master
+            '''
+        }
+      }
+    }
+  }
+  environment {
+    npm_config_cache = 'npm-cache'
+  }
+}
---
+++ terraform_temlate1.tf
@@ -0,0 +1,155 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+	access_key = "${var.access_key}"
+  	secret_key = "${var.secret_key}"
+  	region     = "us-east-1"
+}
+
+data "aws_ami" "node_app_ami" {
+	most_recent = true
+
+
+	filter {
+		name = "name"
+		values = ["packer-example*"]
+	}
+
+	filter {
+		name = "virtualization-type"
+		values = ["hvm"]
+	}
+
+	owners = ["185931747856"]
+}
+
+
+resource "aws_launch_configuration" "node_app_lc" {
+	name_prefix   = "terraform-lc-example-node-app"
+	image_id = "${data.aws_ami.node_app_ami.id}"
+	instance_type = "t2.micro"
+	security_groups = ["${aws_security_group.node_app_websg.id}"]
+
+	lifecycle {
+
+		create_before_destroy = true
+	}
+
+}
+
+
+resource "aws_autoscaling_group" "node_app_asg" {
+	name = "terraform-asg-node-${aws_launch_configuration.node_app_lc.name}"
+	launch_configuration = "${aws_launch_configuration.node_app_lc.name}"
+	availability_zones = ["us-east-1a"]
+	min_size = 1
+	max_size = 2
+
+	load_balancers = ["${aws_elb.elb1.id}"]
+	health_check_type = "ELB"
+
+
+
+	lifecycle{
+		create_before_destroy = true
+	
+	}
+
+}
+
+
+
+resource "aws_security_group" "node_app_websg" {
+
+	name = "security-group-for-node-app-websg"
+	ingress {
+
+		from_port = 3000
+		to_port	= 3000
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	lifecycle {
+
+		create_before_destroy = true
+	}
+}
+
+
+
+resource "aws_security_group" "elbsg" {
+
+	name = "Manas-example-elb-sg"
+	ingress {
+
+		from_port = 3000
+		to_port = 3000
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	lifecycle {
+
+		create_before_destroy = true
+
+	}
+}
+
+
+data "aws_availability_zones" "allzones" {}
+
+resource "aws_elb" "elb1" {
+	name = "node-app-manas-node-appelb"
+	availability_zones = ["us-east-1a"]
+	security_groups = ["${aws_security_group.elbsg.id}"]
+
+
+
+	listener {
+		instance_port = 3000
+		instance_protocol = "http"
+		lb_port = 3000
+		lb_protocol = "http"
+	}
+
+	health_check {
+		healthy_threshold = 2
+		unhealthy_threshold = 2
+		timeout = 3
+		target = "HTTP:3000/"
+		interval = 30
+	}
+
+
+	cross_zone_load_balancing = true
+	idle_timeout = 400
+	connection_draining = true
+	connection_draining_timeout = 400
+
+	tags {
+		name = "terraform-elb-nodeapp"
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
