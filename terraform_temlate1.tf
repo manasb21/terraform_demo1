9ee42e4a181fa17e2e3d7636474f9d315d1f0376
root <root@ip-172-31-14-238.us-west-1.compute.internal>
2018-05-15 05:58:35 +0000

    terraform integration commit version 11

--- terraform_temlate1.tf
+++ terraform_temlate1.tf
@@ -65,8 +65,8 @@ resource "aws_security_group" "node_app_websg" {
 	name = "security-group-for-node-app-websg"
 	ingress {
 
-		from_port = 3000
-		to_port	= 3000
+		from_port = 80
+		to_port	= 80
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
@@ -84,8 +84,8 @@ resource "aws_security_group" "elbsg" {
 	name = "Manas-example-elb-sg"
 	ingress {
 
-		from_port = 3000
-		to_port = 3000
+		from_port = 80
+		to_port = 80
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
@@ -115,9 +115,9 @@ resource "aws_elb" "elb1" {
 
 
 	listener {
-		instance_port = 3000
+		instance_port = 80
 		instance_protocol = "http"
-		lb_port = 3000
+		lb_port = 80
 		lb_protocol = "http"
 	}
 
@@ -125,7 +125,7 @@ resource "aws_elb" "elb1" {
 		healthy_threshold = 2
 		unhealthy_threshold = 2
 		timeout = 3
-		target = "HTTP:3000/"
+		target = "HTTP:80/"
 		interval = 30
 	}
 
